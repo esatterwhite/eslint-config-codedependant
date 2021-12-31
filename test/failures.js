440f6bb3d5fd6a945da25d2d786b6856b3fe17a4
Eric Satterwhite <esatterwhite@wi.rr.com>
2021-12-31 09:20:44 -0600

    chore(deps)!: eslint-plugin-sensible@^3.0.0
    Update the version of sensible plugin and the expected version of eslint
    
    BREAKING CHANGE: eslint version expected to be at version 8

--- test/failures.js
+++ test/failures.js
@@ -3,18 +3,18 @@
 const path = require('path')
 const fs = require('fs')
 const {test, threw} = require('tap')
-const {CLIEngine} = require('eslint')
+const {ESLint} = require('eslint')
 const EOL_CODE = path.join(__dirname, 'fixtures', 'no-eol-fixture')
 
 const readFile = fs.promises.readFile
 test('invalid config', async (t) => {
   const code = await readFile(EOL_CODE, 'utf8')
-  const cli = new CLIEngine({
+  const linter = new ESLint({
     useEslintrc: false
-  , configFile: 'eslintrc.json'
+  , overrideConfigFile: '.eslintrc.json'
   })
 
-  const result = cli.executeOnText(code)
+  const [result] = await linter.lintText(code)
   t.equal(result.errorCount, 1, 'new line missing')
-  t.equal(result.results[0].messages[0].ruleId, 'eol-last', 'missing newline')
+  t.equal(result.messages[0].ruleId, 'eol-last', 'missing newline')
 }).catch(threw)
